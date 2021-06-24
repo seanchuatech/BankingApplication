e5c076191b95cb3c83dec76832e9189c60d2fae0
fate <fate@DESKTOP-39CGHJP>
2021-06-24 12:47:35 +0800

    update

--- BankingApplication/src/BankingApplication.java
+++ BankingApplication/src/BankingApplication.java
@@ -4,6 +4,7 @@ public class BankingApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		BankAccount obj1 = new BankAccount("Sean", "556644");
 		obj1.showMenu();
 
